ccaf48706e38de4d7b6a0f056044ff35ead6a9c8
insissiblecat <alxnqueen@gmail.com>
2023-07-21 17:46:37 +0300

    typeorm to mongoose changed

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,14 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { OrderModule } from './order/order.module';
 import { ContractService } from './contract/contract.service';
 import { EthersService } from './ethers/ethers.service';
 import { EventListenerService } from './event-listener/event-listener.service';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mongodb',
-      url: 'mongodb://localhost:27017/orderbook',
-      autoLoadEntities: true,
-      synchronize: true,
-    }),
+    MongooseModule.forRoot('mongodb://localhost/orderbook'),
     ConfigModule.forRoot(),
     OrderModule,
   ],
---
+++ src/order/order.schema.ts
@@ -0,0 +1,62 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument, Types } from 'mongoose';
+
+export enum OrderType {
+  LIMIT = 'limit',
+  MARKET = 'market',
+}
+
+export enum OrderSide {
+  BUY = 'buy',
+  SELL = 'sell',
+}
+
+export enum OrderStatus {
+  ACTIVE = 'active',
+  PARTIALLY_FILLED = 'partially filled',
+  FILLED = 'filled',
+  CANCELLED = 'cancelled',
+}
+
+export type OrderDocument = HydratedDocument<Order>;
+
+@Schema()
+export class Order {
+  @Prop()
+  id: string;
+
+  @Prop()
+  type: OrderType;
+
+  @Prop()
+  status: OrderStatus;
+
+  @Prop()
+  tokenToSell: string;
+
+  @Prop()
+  tokenToBuy: string;
+
+  @Prop()
+  amountToSell: string;
+
+  @Prop()
+  amountToBuy: string;
+
+  @Prop()
+  amountLeftToFill: string;
+
+  @Prop()
+  fees: string;
+
+  @Prop()
+  user: string;
+
+  @Prop([{ type: Types.ObjectId, ref: 'Order' }])
+  childOrders: Order[];
+
+  @Prop([{ type: Types.ObjectId, ref: 'Order' }])
+  parentOrder: Order;
+}
+
+export const OrderSchema = SchemaFactory.createForClass(Order);
